6cd79ba7771e6ac50b6f17e0f56b828c022879fc
Razvan Onofrei <razwan.me@gmail.com>
2013-05-21 15:39:43 +0300

    initial commit

--- assets/config.min.rb
+++ assets/config.min.rb
@@ -1,8 +1,8 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "dist"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
