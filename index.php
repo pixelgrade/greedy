45c9f23f07e9aafb5dc67ca2edcd482eb3654138
razwan <razwan.me@gmail.com>
2013-05-28 02:25:56 +0300

    remux

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <title></title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans|Roboto+Slab:700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="assets/css/style.css"/>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+    <script type="text/javascript" src="assets/js/vendor/respons.js"></script>
 </head>
 <body>
     <div class="container show-grid">
         <div class="row">
-            <div class="small-span12 hand-span6">span</div>
+            <div class="span12 hand-span3 lap-span2 desk-span1">Nu nu nu</div>
+            <div class="span12 hand-span3 hand-push3 hand-offset3 lap-span2 lap-push2 lap-offset2 desk-span1 desk-push1 desk-offset1">Da da da</div>
         </div>
         <h1>My Bad</h1>
         <p>Tonight's the night. And it's going to happen again and again. It has to happen. This man is a knight in shining armor. I'm not the monster he wants me to be. So I'm neither man nor beast. I'm something new entirely. With my own set of rules. I'm Dexter. Boo. Somehow, I doubt that. You have a good heart, Dexter. I'm going to tell you something that I've never told anyone before.</p>
